d8211122e4bc9d7a0ee08bb370737e9c2cfa83ba
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-26 11:18:17 +0530

    added first repo

---
+++ Firstproject/src/first/oneclass.java
@@ -0,0 +1,14 @@
+package first;
+
+public class oneclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i=10;
+       for(i=0;i<=10;i++) {
+    	   System.out.println(i);
+       }
+       
+	}
+
+}
