d4b076d20e2c64ef3ab6075138ae79f4b5411c3d
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-26 12:28:02 +0530

    added second class

---
+++ Firstproject/src/first/Secondclass.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i=5;
+       for(i=0;i<=5;i++) {
+    	   for(int j =1;j<=5;j++) {
+    	   System.out.println("*");
+       }
+	}
+	}
+
+}
